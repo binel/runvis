7819507ed4c31c6565c7624892a3a849da62e449
Ben Nelson <binelson13@gmail.com>
2024-05-09 19:48:47 -0500

    Trying to scale the image so the route fits the whole screen

--- runvis.c
+++ runvis.c
@@ -7,15 +7,22 @@
 
 #define BUFFER_SIZE 1024
 
-#define MAX_COORDS 1000
-
 typedef struct {
 	double lat;
 	double lon;
+	struct Coordinates* next;
 } Coordinates;
 
+typedef struct {
+	double maxLat;
+	double minLat; 
+	double maxLon;
+	double minLon;
+} RunBounds;
+
 Coordinates* parse_gpx(char* filename);
-Coordinates parse_coords_from_line(const char* line);
+Coordinates* parse_coords_from_line(const char* line);
+RunBounds get_bounding_box(Coordinates* coords);
 
 int main(int argc, char* argv[]) {
 
@@ -24,10 +31,29 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	
-	Coordinates* coords = parse_gpx(argv[1]);
-
+	Coordinates* curCoord = parse_gpx(argv[1]);
+	Coordinates* firstCoord = curCoord;
+	
+	RunBounds box = get_bounding_box(firstCoord);
 	const int screenWidth = 800;
 	const int screenHeight = 800;
+	const int padding = 25;
+	double latRange = box.maxLat - box.minLat;
+	double lonRange = box.maxLon - box.minLon;
+	
+	double degreePerPixel;
+	
+	if (latRange > lonRange){
+		degreePerPixel = latRange / (screenWidth - (padding * 2));
+	}
+	else {
+		degreePerPixel = lonRange / (screenHeight - (padding * 2));
+	}
+	
+	
+	//double lonPerPixel = 
+	
+	//printf("latRange: %f, lonRange: %f",latRange, lonRange);
 	
 	InitWindow(screenWidth, screenHeight, "Test");
 	
@@ -37,28 +63,27 @@ int main(int argc, char* argv[]) {
 		BeginDrawing();
 		
 		ClearBackground(BLACK);
-		int lastxpos = 800 - (int)round((coords[0].lat - 41.7) * 10000);
-		int lastypos = 800 - (int)round(fabs(coords[0].lon + 91.6) * 10000);
-		for (int i = 1; i < MAX_COORDS; i++)
+		int lastxpos = (screenWidth - padding) - (int)round((box.maxLat - curCoord->lat) / degreePerPixel);
+		int lastypos = (screenHeight - padding) - (int)round((box.maxLon - curCoord->lon) / degreePerPixel);
+		while(curCoord->next != NULL)
 		{
-			if (coords[i].lat == 0 && coords[i].lon == 0)
+			if (curCoord->lat == 0 && curCoord->lon == 0)
 			{
 				break;
 			}
-			int xpos = 800 - (int)round((coords[i].lat - 41.7) * 10000);
-			int ypos = 800- (int)round(fabs(coords[i].lon + 91.6) * 10000);
+			int xpos = (screenWidth - padding) - (int)round((box.maxLat - curCoord->lat) / degreePerPixel);
+			int ypos = (screenWidth - padding) - (int)round((box.maxLon - curCoord->lon) / degreePerPixel);
 			
-
-			//TraceLog(LOG_INFO, "%d,%d", xpos, ypos);
 			DrawLine(lastypos, lastxpos, ypos, xpos, RED);
 			lastxpos = xpos;
 			lastypos = ypos;
+			curCoord = curCoord->next;
 		}
-		
+		curCoord = firstCoord;
 		EndDrawing();
 	}
 	
-	free(coords);
+	//free(coords);
 	CloseWindow();
 	
     return 0;
@@ -67,31 +92,36 @@ int main(int argc, char* argv[]) {
 // extremely bad gpx parsing :) 
 
 Coordinates* parse_gpx(char* filename) {
-	Coordinates* coords = malloc(MAX_COORDS * sizeof(Coordinates));
-	int coordCount = 0;
+	Coordinates* curCord; 
+	Coordinates* firstCord;
 	FILE* file = fopen(filename, "r");
 	
 	char buffer[BUFFER_SIZE];
 	while (fgets(buffer, BUFFER_SIZE, file)) {
 		if (strstr(buffer, "<trkpt")) {
-			coords[coordCount++] = parse_coords_from_line(buffer);
-			
-			if (coordCount >= MAX_COORDS) { 
-				break;
+			if (curCord == NULL)
+			{
+				curCord = parse_coords_from_line(buffer);
+				firstCord = curCord;
+			}
+			else 
+			{
+				curCord->next = parse_coords_from_line(buffer);
+				curCord = curCord->next;
 			}
 		}
 	}
 	
 	fclose(file);
-	return coords;
+	return firstCord;
 }
 
-Coordinates parse_coords_from_line(const char* line) {
+Coordinates* parse_coords_from_line(const char* line) {
     const char* lat_start = strstr(line, "lat=\"");
     const char* lon_start = strstr(line, "lon=\"");
 
-	Coordinates loc;
-
+	Coordinates* loc = malloc(sizeof(Coordinates));
+	
     if (lat_start != NULL && lon_start != NULL) {
         lat_start += 5;  // Move past "lat=\""
         lon_start += 5;  // Move past "lon=\""
@@ -112,10 +142,46 @@ Coordinates parse_coords_from_line(const char* line) {
         longitude[i] = '\0';
         char* endptr; 
         
-        loc.lat = strtod(latitude, &endptr);
-        loc.lon = strtod(longitude, &endptr);
+        loc->lat = strtod(latitude, &endptr);
+        loc->lon = strtod(longitude, &endptr);
     }
     
     return loc;
 }
 
+RunBounds get_bounding_box(Coordinates* coords) {
+	RunBounds box;
+	
+	box.maxLat = coords->lat;
+	box.minLat = coords->lat;
+	box.maxLon = coords->lon;
+	box.minLon = coords->lon;
+	
+	coords = coords->next;
+	
+	while(coords->next != NULL) {
+		if (coords->lat > box.maxLat)
+		{
+			box.maxLat = coords->lat;
+		}
+		
+		if (coords->lat < box.minLat)
+		{
+			box.minLat = coords->lat;
+		}
+		
+		if (coords->lon > box.maxLon)
+		{
+			box.maxLon = coords->lon;
+		}
+		
+		if (coords->lon < box.minLon)
+		{
+			box.minLon = coords->lon;
+		}
+		
+		coords = coords->next;
+	}
+	
+	return box;
+}
